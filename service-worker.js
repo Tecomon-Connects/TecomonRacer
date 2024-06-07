fbcea1fe83b5e8e2ddd241d5959c07127032a9dd
Paul Paffe <paul.paffe@gmx.de>
2024-06-07 21:59:05 +0200

    test workbox sw

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,20 @@
-// Path: service-worker.js
+// // Path: service-worker.js
 
-self.addEventListener("install", (event) => {});
+// self.addEventListener("install", (event) => {});
 
-self.addEventListener("fetch", function (event) {
-  event.respondWith(fetch(event.request));
-});
+// self.addEventListener("fetch", function (event) {
+//   event.respondWith(fetch(event.request));
+// });
 
-self.addEventListener("activate", (event) => {});
+// self.addEventListener("activate", (event) => {});
+// Replace 3.6.3 with the current version number of Workbox.
+importScripts(
+  "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js"
+);
+
+workbox.routing.registerRoute(
+  new RegExp(".png$"),
+  workbox.strategies.cacheFirst({
+    cacheName: "images-cache",
+  })
+);
